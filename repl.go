b7051b318e4fc529abfdf3c95cf5bdcf438ccb44
Alan Night <a.nightails@gmail.com>
2025-08-30 15:20:54 -0500

    Add help command

--- repl.go
+++ repl.go
@@ -7,11 +7,30 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
 }
 
 func startRelp() {
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Show how to use Pokedexs",
+			callback:    commandHelp,
+		},
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
@@ -22,6 +41,31 @@ func startRelp() {
 			continue
 		}
 
-		fmt.Printf("Your command was: %s\n", input[0])
+		command := input[0]
+		if cli, exist := commands[command]; exist {
+			if err := cli.callback(); err != nil {
+				fmt.Println(fmt.Errorf("error: %v", err))
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	message := `
+Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex
+`
+	fmt.Println(message)
+	return nil
 }
