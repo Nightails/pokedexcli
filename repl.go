5c2727c78e9ebbbb3c3bbe3bee762cdc73d4a53f
Alan Night <a.nightails@gmail.com>
2025-08-30 13:59:25 -0500

    Add repl and clean input function

---
+++ repl.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	return strings.Fields(strings.ToLower(text))
+}
+
+func startRelp() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", input[0])
+	}
+}
---
+++ repl_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Pikachu Eevee",
+			expected: []string{"pikachu", "eevee"},
+		},
+		{
+			input:    "",
+			expected: []string{},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("got %v, expect %v", actual, c.expected)
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("got %s, expect %s", word, expectedWord)
+			}
+		}
+	}
+}
